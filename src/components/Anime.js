bd047932059fee06a3a56df1e08cfe0f93f5c64f
Abdelrahman Mohammad Al Daour <Daour.995@outlook.com>
2021-06-06 13:41:35 +0300

    Finsihed Modal for the components

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -1,32 +1,81 @@
 import React, { Component } from 'react';
-import { Card, Button, CardGroup, CardColumns } from 'react-bootstrap';
+import { Card, Button, CardGroup, CardColumns, Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export class Anime extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayModal: false,
+      item: {},
+    };
+  }
+
+  showModal = (item) => {
+    this.setState({
+      displayModal: true,
+      item: item,
+    });
+  };
+  hideModal = () => {
+    this.setState({
+      displayModal: false,
+    });
+  };
   render() {
     return (
-      <CardColumns>
-        {this.props.animeData.map((item2) => {
-          console.log('hello Anime');
-          return item2.map((item, idx) => {
-            return (
-              <CardGroup key={idx} style={{ width: '18rem' }}>
-                <Card>
-                  <Card.Img variant="top" src={item.image} />
+      <div>
+        <CardColumns>
+          {this.props.animeData.map((item2) => {
+            console.log('hello Anime');
+            return item2.map((item, idx) => {
+              return (
+                <CardGroup key={idx} style={{ width: '18rem' }}>
+                  <Card onClick={() => this.showModal(item)}>
+                    <Card.Img variant="top" src={item.image} />
 
-                  <Card.Body>
-                    <Card.Title> {item.title}</Card.Title>
-                    {/* <Card.Text>{item.description}</Card.Text>
+                    <Card.Body>
+                      <Card.Title> {item.title}</Card.Title>
+                      
+                    </Card.Body>
+                  </Card>
+                </CardGroup>
+              );
+            });
+          })}
+        </CardColumns>
 
-                    <Button variant="primary">Delete</Button>
-                    <Button variant="primary">Feedback</Button> */}
-                  </Card.Body>
-                </Card>
-              </CardGroup>
-            );
-          });
-        })}
-      </CardColumns>
+        <Modal show={this.state.displayModal} onHide={this.hideModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.state.item.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src={this.state.item.image} alt={this.state.item.title}></img>
+            <br></br>
+            <p>Description: {this.state.item.description}</p>
+
+            <p>Date: {this.state.item.date}</p>
+
+            <p>Type: {this.state.item.type}</p>
+
+            <p>Category: {this.state.item.category}</p>
+
+            <p>Episodes: {this.state.item.episodes}</p>
+
+            <p>
+              Score & Rating: {this.state.item.score} ,{this.state.item.rate}
+            </p>
+
+            <a href={this.state.item.watchURL}>Watch Here</a>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.hideModal}>
+              Close
+            </Button>
+            <Button variant="primary">Add to Favorite</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   }
 }
