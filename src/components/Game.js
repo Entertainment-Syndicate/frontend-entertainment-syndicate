731ed8363277e9d5653d31c3db90df3f241864f9
dana-younis <younisdana7@gmail.com>
2021-06-06 15:53:24 +0300

    finish scroll

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-import { Card, Button, CardGroup, CardColumns, Modal } from 'react-bootstrap';
+import {
+  Card,
+  Button,
+  CardGroup,
+  CardColumns,
+  Modal,
+  CardDeck,
+} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'semantic-ui-css/semantic.min.css';
+import 'react-multi-carousel/lib/styles.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 export class Game extends Component {
   constructor(props) {
     super(props);
@@ -25,29 +35,97 @@ export class Game extends Component {
   render() {
     return (
       <div>
-        <CardColumns>
-          {this.props.gameData.map((item2) => {
-            console.log('hello game');
-            return item2.map((item, idx) => {
-              return (
-                <CardGroup key={idx} style={{ width: '18rem' }}>
-                  <Card onClick={() => this.showModal(item)}>
-                    <Card.Img variant="top" src={item.image} />
+        {this.props.showAnime && this.props.showMovie && this.props.showGame ? (
+          <Carousel
+            additionalTransfrom={0}
+            arrows
+            autoPlay
+            autoPlaySpeed={3000}
+            centerMode={true}
+            className=""
+            containerClass="container-with-dots"
+            customTransition="all 1s linear"
+            dotListClass=""
+            draggable
+            focusOnSelect={false}
+            infinite
+            itemClass=""
+            keyBoardControl
+            minimumTouchDrag={80}
+            renderButtonGroupOutside={false}
+            renderDotsOutside={false}
+            responsive={{
+              desktop: {
+                breakpoint: {
+                  max: 3000,
+                  min: 1024,
+                },
+                items: 3,
+                partialVisibilityGutter: 40,
+              },
+              mobile: {
+                breakpoint: {
+                  max: 464,
+                  min: 0,
+                },
+                items: 1,
+                partialVisibilityGutter: 30,
+              },
+              tablet: {
+                breakpoint: {
+                  max: 1024,
+                  min: 464,
+                },
+                items: 2,
+                partialVisibilityGutter: 30,
+              },
+            }}
+            showDots={false}
+            sliderClass=""
+            slidesToSlide={2}
+            swipeable
+            transitionDuration={300}
+          >
+            {this.props.gameData.map((item2) => {
+              console.log('hello Anime');
+              return item2.map((item, idx) => {
+                return (
+                  <CardGroup key={idx} style={{ width: '18rem' }}>
+                    <Card onClick={() => this.showModal(item)}>
+                      <Card.Img variant="top" src={item.image} />
 
-                    <Card.Body>
-                      <Card.Title> {item.title}</Card.Title>
-                      
-                    </Card.Body>
-                  </Card>
-                </CardGroup>
-              );
-            });
-          })}
-        </CardColumns>
+                      <Card.Body>
+                        <Card.Title> {item.title}</Card.Title>
+                      </Card.Body>
+                    </Card>
+                  </CardGroup>
+                );
+              });
+            })}
+          </Carousel>
+        ) : (
+          <div>
+            <CardDeck>
+              {this.props.gameData.map((item2) => {
+                console.log('hello Anime');
+                return item2.map((item, idx) => {
+                  return (
+                    <CardGroup key={idx} style={{ width: '18rem' }}>
+                      <Card onClick={() => this.showModal(item)}>
+                        <Card.Img variant="top" src={item.image} />
 
+                        <Card.Body>
+                          <Card.Title> {item.title}</Card.Title>
+                        </Card.Body>
+                      </Card>
+                    </CardGroup>
+                  );
+                });
+              })}
+            </CardDeck>
+          </div>
+        )}
 
-    
-   
         <Modal show={this.state.displayModal} onHide={this.hideModal}>
           <Modal.Header closeButton>
             <Modal.Title>{this.state.item.title}</Modal.Title>
@@ -70,7 +148,7 @@ export class Game extends Component {
             <p>Platform: {this.state.item.platform}</p>
 
             <p>
-            Publisher & Developer: {this.state.item.publisher} ,
+              Publisher & Developer: {this.state.item.publisher} ,
               {this.state.item.developer}
             </p>
 
