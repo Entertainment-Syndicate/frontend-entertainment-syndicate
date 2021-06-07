edd198ae23d368882edd8a7d6dc99d38b9b80cfa
Yahia Qous <yahiaqous@gmail.com>
2021-06-07 18:25:23 +0300

    watchList

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,21 +6,22 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      randomAnime: [],
-
-      randomMovie: [],
-      randomGame: [],
+      randomAnime1: [],
+      randomAnime2: [],
+      randomMovie1: [],
+      randomMovie2: [],
     };
   }
   componentDidMount = async () => {
     axios
       .get(`https://api.jikan.moe/v3/search/anime?q=&genre=1&order_by=score`)
       .then((res) => {
-        let animeArr = res.data.results.splice(0, 10);
-        let randomNumber = Math.floor(Math.random() * 10);
-
+        let animeArr = res.data.results.splice(0, 20);
+        let randomNumber1 = Math.floor(Math.random() * 10);
+        let randomNumber2 = Math.floor(Math.random() * (20 - 10 + 1)) + 10;
         this.setState({
-          randomAnime: animeArr[randomNumber],
+          randomAnime1: animeArr[randomNumber1],
+          randomAnime2: animeArr[randomNumber2],
         });
       });
 
@@ -29,62 +30,70 @@ class Home extends Component {
         `https://api.themoviedb.org/3/discover/movie?api_key=32705b2157d089bfa091a1f46fd73813&with_genres=28`
       )
       .then((res) => {
-        let MovieArr = res.data.results.splice(0, 10);
-        let randomNumber = Math.floor(Math.random() * 10);
-
+        let movieArr = res.data.results.splice(0, 20);
+        let randomNumber1 = Math.floor(Math.random() * 10);
+        let randomNumber2 = Math.floor(Math.random() * (20 - 10 + 1)) + 10;
         this.setState({
-          randomMovie: MovieArr[randomNumber],
+          randomMovie1: movieArr[randomNumber1],
+          randomMovie2: movieArr[randomNumber2],
         });
       });
 
-    let res = await axios.get(
-      `https://www.freetogame.com/api/games?category=action`
-    );
+    // let res = await axios.get(
+    //   `https://www.freetogame.com/api/games?category=action`
+    // );
 
-    //   ((res) => {
-    console.log(' inside game', res);
+    // // ((res) => {
+    // console.log(' inside game', res);
     // let gameArr = res.data.splice(0, 10);
     // let randomNumber = Math.floor(Math.random() * 10);
     // console.log(gameArr);
     // this.setState({
     //   randomGame: gameArr[randomNumber],
     // });
-    //   })
-    //   .catch((error) => console.log(error));
+    // // })
+    // // .catch((error) => console.log(error));
   };
 
   render() {
     return (
       <div>
-        <CardGroup style={{ width: '18rem' }}>
+        <CardGroup style={{ width: '40rem' }}>
           <Card>
-            <Card.Img variant="top" src={this.state.randomAnime.image_url} />
+            <Card.Img variant="top" src={this.state.randomAnime1.image_url} />
 
             <Card.Body>
-              <Card.Title> {this.state.randomAnime.title}</Card.Title>
+              <Card.Title> {this.state.randomAnime1.title}</Card.Title>
             </Card.Body>
           </Card>
-        </CardGroup>
 
-        <CardGroup style={{ width: '18rem' }}>
+          <Card>
+            <Card.Img variant="top" src={this.state.randomAnime2.image_url} />
+
+            <Card.Body>
+              <Card.Title> {this.state.randomAnime2.title}</Card.Title>
+            </Card.Body>
+          </Card>
+
           <Card>
             <Card.Img
               variant="top"
-              src={`https://image.tmdb.org/t/p/w500${this.state.randomMovie.poster_path}`}
+              src={`https://image.tmdb.org/t/p/w500${this.state.randomMovie1.poster_path}`}
             />
 
             <Card.Body>
-              <Card.Title> {this.state.randomMovie.title}</Card.Title>
+              <Card.Title> {this.state.randomMovie1.title}</Card.Title>
             </Card.Body>
           </Card>
-        </CardGroup>
 
-        <CardGroup style={{ width: '18rem' }}>
           <Card>
-            <Card.Img variant="top" src={this.state.randomGame.thumbnail} />
+            <Card.Img
+              variant="top"
+              src={`https://image.tmdb.org/t/p/w500${this.state.randomMovie2.poster_path}`}
+            />
 
             <Card.Body>
-              <Card.Title> {this.state.randomGame.title}</Card.Title>
+              <Card.Title> {this.state.randomMovie2.title}</Card.Title>
             </Card.Body>
           </Card>
         </CardGroup>
