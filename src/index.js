6f4b79beaedd52ca63c434c1ac78c958a9b2553e
Mahmoud-Saadeh <mahmoud.saadeh998@gmail.com>
2021-06-08 18:34:31 +0300

    edited the navbar

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import { Auth0Provider } from '@auth0/auth0-react';
-
+import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
-  <Auth0Provider
-    domain={'dev-ikja9n-1.eu.auth0.com'}
-    clientId={'u4ybZfNdrk1ndIqGj65e5Rq3ZVKoqi42'}
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
+  <BrowserRouter>
+    <Auth0Provider
+      domain={'dev-ikja9n-1.eu.auth0.com'}
+      clientId={'u4ybZfNdrk1ndIqGj65e5Rq3ZVKoqi42'}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
