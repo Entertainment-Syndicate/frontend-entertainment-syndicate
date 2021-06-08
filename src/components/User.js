efde9e375ecd4ce95e6df702ec5c9ab79c1a185d
hibasalem <salemhiba.hs@gmail.com>
2021-06-08 21:31:10 +0300

    header situation

--- src/components/User.js
+++ src/components/User.js
@@ -2,31 +2,33 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import FavoriteList from './FavoriteList';
 import '../CSS/FavoriteList.css';
+import './user.css';
 
 const User = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
-  // Creating function for removing (related to Database)
+  const { loginWithRedirect } = useAuth0();
 
-  // Creating function for feedback (related to Database)
-
-  // if (isLoading) {
-  //   return <div>Loading ...</div>;
-  // }
-
-  return (
-    isAuthenticated && (
-      <div>
-        <div className="userHeader">
-          <img id="profilePicture" src={user.picture} alt={user.name} />
-          <h2> Name: {user.name}</h2>
-          <p> Email: {user.email}</p>
-        </div>
-        <div className="FavCards">
-          <FavoriteList />
-        </div>
+  return isAuthenticated ? (
+    <div>
+      <div className="userHeader">
+        <img id="profilePicture" src={user.picture} alt={user.name} />
+        <h2 className="userh2"> Name: {user.name}</h2>
+        <p> Email: {user.email}</p>
       </div>
-    )
+      <div className="FavCards">
+        <FavoriteList />
+      </div>
+    </div>
+  ) : (
+    <div>
+      <div className="userborder">
+        <h3 className="userh3">Please Log In </h3>
+        <button className="userlogin" onClick={() => loginWithRedirect()}>
+          Log In
+        </button>
+      </div>
+    </div>
   );
 };
 
