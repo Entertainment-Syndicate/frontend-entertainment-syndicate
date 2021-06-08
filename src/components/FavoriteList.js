7b140f8e94211492056194b61a55e80719a8cc86
hibasalem <salemhiba.hs@gmail.com>
2021-06-08 11:52:18 +0300

    things

--- src/components/FavoriteList.js
+++ src/components/FavoriteList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Button, CardGroup, CardDeck } from 'react-bootstrap';
+import { Card, Button, CardGroup, CardDeck, Accordion } from 'react-bootstrap';
 import axios from 'axios';
 import { withAuth0 } from '@auth0/auth0-react';
 import Feedback from './Feedback';
@@ -136,22 +136,45 @@ export class FavoriteList extends Component {
 
                       <Card.Body>
                         <Card.Title> {item.title}</Card.Title>
-                        <Card.Text>Description: {item.description}</Card.Text>
-                        <Card.Text>Date: {item.date}</Card.Text>
-                        <Card.Text>Type: {item.type}</Card.Text>
-                        <Card.Text>Category: {item.category}</Card.Text>
-                        <Card.Text>Episodes: {item.episodes}</Card.Text>
-                        <Card.Text>Score: {item.score}</Card.Text>
-                        <Card.Text>Rating: {item.rate}</Card.Text>
-                        <a href={item.watchURL}>Watch Here</a>
                       </Card.Body>
-                      <Card.Footer>
+                      <Accordion defaultActiveKey="0">
+                        <Card>
+                          <Accordion.Toggle as={Card.Header} eventKey="0">
+                            about the Anime
+                          </Accordion.Toggle>
+                          <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                              <Card.Text>
+                                Description: {item.description}
+                              </Card.Text>
+                              <Card.Text>Date: {item.date}</Card.Text>
+                              <Card.Text>Type: {item.type}</Card.Text>
+                              <Card.Text>Category: {item.category}</Card.Text>
+                              <Card.Text>Episodes: {item.episodes}</Card.Text>
+                              <Card.Text>Score: {item.score}</Card.Text>
+                              <Card.Text>Rating: {item.rate}</Card.Text>
+                              <a href={item.watchURL}>Watch Here</a>
+                            </Card.Body>
+                          </Accordion.Collapse>
+                        </Card>
+                        <Card>
+                          <Accordion.Toggle as={Card.Header} eventKey="1">
+                            Your Review
+                          </Accordion.Toggle>
+                          <Accordion.Collapse eventKey="1">
+                            <Card.Body>
+                              <Card.Text>{item.feedback}</Card.Text>
+                            </Card.Body>
+                          </Accordion.Collapse>
+                        </Card>
+                      </Accordion>
+                      <Card.Body>
                         {/* 7  , 4th req */}
                         <Button
                           variant="danger"
                           onClick={() => this.delete(idx, item.type)}
                         >
-                          Delete
+                          Remove Anime
                         </Button>
                         {/* add input field  on submiit and a place to put it and the  && */}
                         <Button
@@ -164,9 +187,9 @@ export class FavoriteList extends Component {
                             )
                           }
                         >
-                          feedback
+                          Review
                         </Button>
-                      </Card.Footer>
+                      </Card.Body>
                     </Card>
                   </CardGroup>
                 );
@@ -187,21 +210,50 @@ export class FavoriteList extends Component {
 
                       <Card.Body>
                         <Card.Title> {item.title}</Card.Title>
-                        <Card.Text>Description: {item.description}</Card.Text>
-                        <Card.Text>Date: {item.date}</Card.Text>
-                        <Card.Text>Type: {item.type}</Card.Text>
-                        <Card.Text>Category: {item.category}</Card.Text>
-                        <Card.Text>Vote Average: {item.voteAverage}</Card.Text>
-                        <Card.Text>Vote Count: {item.voteCount}</Card.Text>
-                        <Card.Text>Popularity: {item.popularity}</Card.Text>
                       </Card.Body>
-                      <Card.Footer>
+                      <Accordion defaultActiveKey="0">
+                        <Card>
+                          <Accordion.Toggle as={Card.Header} eventKey="0">
+                            about the Movie
+                          </Accordion.Toggle>
+                          <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                              <Card.Text>
+                                Description: {item.description}
+                              </Card.Text>
+                              <Card.Text>Date: {item.date}</Card.Text>
+                              <Card.Text>Type: {item.type}</Card.Text>
+                              <Card.Text>Category: {item.category}</Card.Text>
+                              <Card.Text>
+                                Vote Average: {item.voteAverage}
+                              </Card.Text>
+                              <Card.Text>
+                                Vote Count: {item.voteCount}
+                              </Card.Text>
+                              <Card.Text>
+                                Popularity: {item.popularity}
+                              </Card.Text>
+                            </Card.Body>
+                          </Accordion.Collapse>
+                        </Card>
+                        <Card>
+                          <Accordion.Toggle as={Card.Header} eventKey="1">
+                            Your Review
+                          </Accordion.Toggle>
+                          <Accordion.Collapse eventKey="1">
+                            <Card.Body>
+                              <Card.Text>{item.feedback}</Card.Text>
+                            </Card.Body>
+                          </Accordion.Collapse>
+                        </Card>
+                      </Accordion>
+                      <Card.Body>
                         {/* 7  , 4th req */}
                         <Button
                           variant="danger"
                           onClick={() => this.delete(idx, item.type)}
                         >
-                          Delete
+                          Remove Movie
                         </Button>
                         {/* add input field  on submiit and a place to put it and the  && */}
                         <Button
@@ -214,9 +266,9 @@ export class FavoriteList extends Component {
                             )
                           }
                         >
-                          feedback
+                          Review
                         </Button>
-                      </Card.Footer>
+                      </Card.Body>
                     </Card>
                   </CardGroup>
                 );
@@ -236,22 +288,45 @@ export class FavoriteList extends Component {
                       <Card.Img variant="top" src={item.image} />
                       <Card.Body>
                         <Card.Title> {item.title}</Card.Title>
-                        <Card.Text>Description: {item.description}</Card.Text>
-                        <Card.Text>Date: {item.date}</Card.Text>
-                        <Card.Text>Type: {item.type}</Card.Text>
-                        <Card.Text>Category: {item.category}</Card.Text>
-                        <Card.Text>Platform: {item.platform}</Card.Text>
-                        <Card.Text>Publisher: {item.publisher}</Card.Text>
-                        <Card.Text>Developer: {item.developer}</Card.Text>
-                        <a href={item.installingURL}>Install from Here</a>
                       </Card.Body>
-                      <Card.Footer>
+                      <Accordion defaultActiveKey="0">
+                        <Card>
+                          <Accordion.Toggle as={Card.Header} eventKey="0">
+                            about the Game
+                          </Accordion.Toggle>
+                          <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                              <Card.Text>
+                                Description: {item.description}
+                              </Card.Text>
+                              <Card.Text>Date: {item.date}</Card.Text>
+                              <Card.Text>Type: {item.type}</Card.Text>
+                              <Card.Text>Category: {item.category}</Card.Text>
+                              <Card.Text>Platform: {item.platform}</Card.Text>
+                              <Card.Text>Publisher: {item.publisher}</Card.Text>
+                              <Card.Text>Developer: {item.developer}</Card.Text>
+                              <a href={item.installingURL}>Install from Here</a>
+                            </Card.Body>
+                          </Accordion.Collapse>
+                        </Card>
+                        <Card>
+                          <Accordion.Toggle as={Card.Header} eventKey="1">
+                            Your Review
+                          </Accordion.Toggle>
+                          <Accordion.Collapse eventKey="1">
+                            <Card.Body>
+                              <Card.Text>{item.feedback}</Card.Text>
+                            </Card.Body>
+                          </Accordion.Collapse>
+                        </Card>
+                      </Accordion>
+                      <Card.Body>
                         {/* 7  , 4th req */}
                         <Button
                           variant="danger"
                           onClick={() => this.delete(idx, item.type)}
                         >
-                          Delete
+                          Remove Game
                         </Button>
                         {/* add input field  on submiit and a place to put it and the  && */}
                         <Button
@@ -264,9 +339,9 @@ export class FavoriteList extends Component {
                             )
                           }
                         >
-                          feedback
+                          Review
                         </Button>
-                      </Card.Footer>
+                      </Card.Body>
                     </Card>
                   </CardGroup>
                 );
